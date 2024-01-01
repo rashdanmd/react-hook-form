659cbe600a63903226a59c2bf3287fb5ddbfba32
rashdanmd <rashdan.developer@gmail.com>
2024-01-01 14:40:25 +0000

    initial setup with zod

--- rhf-zod-server-side/app/page.tsx
+++ rhf-zod-server-side/app/page.tsx
@@ -1,10 +1,12 @@
 import FormWithReactHookForm from "@/components/form-with-react-hook-form";
+import FormWithReactHookFormAndZod from "@/components/form-with-rhf-zod";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormWithReactHookForm />
+      {/* <FormWithReactHookForm /> */}
+      <FormWithReactHookFormAndZod />
     </main>
   );
 }
