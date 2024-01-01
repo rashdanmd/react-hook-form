10c269e5413ccfa76935e36c442c8623a003ab83
rashdanmd <rashdan.developer@gmail.com>
2024-01-01 14:23:21 +0000

    rhf handle submit + errors

--- rhf-zod-server-side/components/form-with-react-hook-form.tsx
+++ rhf-zod-server-side/components/form-with-react-hook-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useForm } from "react-hook-form";
+import type { FieldValues } from "react-hook-form";
 
 export default function FormWithReactHookForm() {
   const {
@@ -9,23 +10,56 @@ export default function FormWithReactHookForm() {
     reset,
     getValues,
   } = useForm();
+
+  const onSubmit = async (data: FieldValues) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    reset();
+  };
+
   return (
-    <form className="flex flex-col gap-y-2">
-      <input type="email" placeholder="Email" className="px-4 py-2 rounded" />
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-2">
+      <input
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="Email"
+        className="px-4 py-2 rounded"
+      />
+      {errors.email && (
+        <p className="text-red-500">{`${errors.email.message}`}</p>
+      )}
 
       <input
+        {...register("password", {
+          required: "Password is required",
+          minLength: {
+            value: 10,
+            message: "Password must be at least 10 characters",
+          },
+        })}
         type="password"
         placeholder="Password"
         className="px-4 py-2 rounded"
       />
+      {errors.password && (
+        <p className="text-red-500">{`${errors.password.message}`}</p>
+      )}
 
       <input
+        {...register("confirmPassword", {
+          required: "Confirm Password is required",
+          validate: (value) =>
+            value === getValues("password") || "Passwords must match",
+        })}
         type="password"
         placeholder="Confirm password"
         className="px-4 py-2 rounded"
       />
+      {errors.confirmPassword && (
+        <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
+      )}
 
       <button
+        disabled={isSubmitting}
         type="submit"
         className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
       >
